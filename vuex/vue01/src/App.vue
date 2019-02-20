2bde48a04d42ce9729094fb762ee3c21af318fdf
pain <1741248769@qq.com>
2019-02-20 20:38:40 +0800

    update vue01 使用mapState、mapGetters、mapActions简化编码

--- vuex/vue01/src/App.vue
+++ vuex/vue01/src/App.vue
@@ -9,27 +9,47 @@
 </template>
 
 <script>
+    // 引入   mapState、mapGetters、mapActions
+    import {mapState, mapGetters, mapActions} from 'vuex'
 
     export default {
         name: 'app',
         computed: {
-            odd_even() {
-                return this.$store.getters.odd_even
-            }
+            // 当映射的计算属性的名称与 state 的子节点名称相同时，我们也可以给 mapState 传一个字符串数组。
+            ...mapState([
+                // 映射 this.count 为 store.state.count
+                'count'
+            ]),
+            // 使用对象展开运算符将 getter 混入 computed 对象中
+            ...mapGetters([
+                'odd_even'
+            ]),
+            // odd_even() {
+            //     return this.$store.getters.odd_even
+            // }
         },
         methods: {
-            increase() {
-                this.$store.dispatch('increase')
-            },
-            decrease() {
-                this.$store.dispatch('decrease')
-            },
-            increaseIfOdd() {
-                this.$store.dispatch('increaseIfOdd')
-            },
-            increaseAsync() {
-                this.$store.dispatch('increaseAsync')
-            }
+            ...mapActions([
+                'increase',
+                'decrease',
+                'increaseIfOdd',
+                'increaseAsync'
+            ]),
+            // ...mapActions({
+            //     add: 'increment' // 将 `this.add()` 映射为 `this.$store.dispatch('increment')`
+            // })
+            // increase() {
+            //     this.$store.dispatch('increase')
+            // },
+            // decrease() {
+            //     this.$store.dispatch('decrease')
+            // },
+            // increaseIfOdd() {
+            //     this.$store.dispatch('increaseIfOdd')
+            // },
+            // increaseAsync() {
+            //     this.$store.dispatch('increaseAsync')
+            // }
         }
     }
 </script>
--- vuex/vue01/src/store.js
+++ vuex/vue01/src/store.js
@@ -3,10 +3,12 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+// 状态对象
 const state = {
     count: 0
 }
 
+// 包含多个对应事件回调函数的对象
 const actions = {
     increase({commit}) {
         commit('increase')
@@ -26,6 +28,7 @@ const actions = {
     }
 }
 
+// 包含多个更改store状态的函数的对象
 const mutations = {
     increase(state) {
         state.count++
@@ -35,6 +38,7 @@ const mutations = {
     }
 }
 
+// 包含多个getter计算属性的函数的对象
 const getters = {
     odd_even(state) {
         return state.count % 2 === 0 ? '偶数' : '奇数'
@@ -42,8 +46,8 @@ const getters = {
 }
 
 export default new Vuex.Store({
-    state,  // 状态对象
-    actions,    // 包含多个对应事件回调函数的对象
-    mutations,  // 包含多个更改store状态的函数的对象
-    getters     // 包含多个getter计算属性的函数的对象
+    state,
+    actions,
+    mutations,
+    getters
 })
