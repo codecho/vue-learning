8370efb5acbdc072e205c401aaf546dd3d51cf8b
pain <1741248769@qq.com>
2019-05-08 21:29:32 +0800

    修正一个错误 修正前：return JSON.parse(localStorage.getItem(todos_key || '[]')) 修正后：return JSON.parse(localStorage.getItem(todos_key) || '[]')

--- vuex/vue02/src/util/storeUtil.js
+++ vuex/vue02/src/util/storeUtil.js
@@ -5,9 +5,9 @@ const todos_key = 'todos'
 export default {
   readTodos() {
     // 从localStorage中读取todos，如果todos为空，则返回一个空数组
-    return JSON.parse(localStorage.getItem(todos_key || '[]'))
+    return JSON.parse(localStorage.getItem(todos_key) || '[]')
   },
   saveTodos(todos) {
-    localStorage.setItem(todos_key,JSON.stringify(todos))
+    localStorage.setItem(todos_key, JSON.stringify(todos))
   }
 }
